2ad4921b70c4fd44d8d20f14c48fa31a3a3e7d70
jhoner dlamarck <jhonerdlamarck@jhoners-MacBook-Pro.local>
2024-04-04 15:05:54 -0500

    services

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,42 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Typography,
+} from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image="/static/images/cards/contemplative-reptile.jpg"
+          title="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Lizard
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small">Share</Button>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+      </Card>
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/requests.js
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const BASE_URL = "https://65f394fe105614e654a0ac9d.mockapi.io/api/v1/animals";
+
+// Function to handle errors
+const handleErrors = (error) => {
+  console.error("An error occurred:", error);
+  throw error;
+};
+
+// Function to handle GET request
+export const getAnimals = async () => {
+  try {
+    const response = await axios.get(BASE_URL);
+    return response.data;
+  } catch (error) {
+    handleErrors(error);
+  }
+};
+
+// Function to handle POST request
+export const addAnimal = async (animal) => {
+  try {
+    const response = await axios.post(BASE_URL, animal);
+    return response.data;
+  } catch (error) {
+    handleErrors(error);
+  }
+};
+
+// Function to handle PUT request
+export const updateAnimal = async (id, animal) => {
+  try {
+    const response = await axios.put(`${BASE_URL}/${id}`, animal);
+    return response.data;
+  } catch (error) {
+    handleErrors(error);
+  }
+};
+
+// Function to handle DELETE request
+export const deleteAnimal = async (id) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (error) {
+    handleErrors(error);
+  }
+};
